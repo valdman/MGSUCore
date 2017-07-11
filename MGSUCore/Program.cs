e1c9163ddba97bc218dc6938c9bf14c65617c765
Boris Valdman <boris.valdman@live.ru>
2017-07-12 00:05:09 +0300

    iis integration

--- MGSUCore/Program.cs
+++ MGSUCore/Program.cs
@@ -10,6 +10,7 @@ namespace MGSUCore
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
                 .UseStartup<Startup>()
                 .UseApplicationInsights()
                 .Build();
