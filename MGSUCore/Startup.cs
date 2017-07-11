514093e5f3a157ca132879cbe432bceca933ea73
Boris Valdman <boris.valdman@live.ru>
2017-07-12 00:05:09 +0300

    iis and other reverse-proxy integration

--- MGSUCore/Startup.cs
+++ MGSUCore/Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.Extensions.Logging;
 using Common.Entities;
 using Common;
 using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.HttpOverrides;
 
 namespace MGSUCore
 {
@@ -97,6 +98,11 @@ namespace MGSUCore
 
             //CORS
             app.UseCors("AllowCors");
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
 
             app.UseMvc();
             
