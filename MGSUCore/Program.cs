96a9471209e48d4dd18e85257321a44fa61fec49
Boris Valdman <boris.valdman@live.ru>
2017-07-19 03:55:53 +0300

    add listening all interfaces

--- MGSUCore/Program.cs
+++ MGSUCore/Program.cs
@@ -8,6 +8,7 @@ namespace MGSUCore
         public static void Main(string[] args)
         {
             var host = new WebHostBuilder()
+                .UseUrls("http://0.0.0.0:5000")
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
